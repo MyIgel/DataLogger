fec91013b855d2a48c8c18666de239cdd8b2a6b6
Igor Scheller <igor.scheller@igorshp.de>
2016-08-13 22:30:29 +0200

    Refactoring of templog.sh

--- client/templog.sh
+++ client/templog.sh
@@ -12,37 +12,51 @@ SERVER='http://log.server.com'
 
 #=============================================
 
+readTemp(){
+    local file=$1
+    # Temperatur des Sensors auslesen
+    local temp=$(grep 't=' "${file}" | awk -F't=' '{print $2}')
+    temp=$(echo "scale=2; $temp / 1000" | bc)
+
+    echo "${temp}"
+}
+
 getTemp(){
+    local success=true
     for file in /sys/bus/w1/devices/10-*/w1_slave; do
 
         # Temperatur des Sensors auslesen
-        temp=$(grep 't=' "${file}" | awk -F't=' '{print $2}')
-        temp=$(echo "scale=2; $temp / 1000" | bc)
+        local temp=$(readTemp "${file}")
         if [ '-1,25' = "{$temp}" ]; then
             # Workaround, wenn der Außensensor einen falschen Wert zurückgibt (hier -1.25)
-            temp=$(grep 't=' "${file}" | awk -F't=' '{print $2}')
-            temp=$(echo "scale=2; $temp / 1000" | bc)
+            temp=$(readTemp "${file}")
         fi
 
-        sensor=$(echo "${file}" |tail -c25|head -c15)
+        local sensor=$(echo "${file}" | awk -F'/' '{print $6}')
 
         # Wert ausgeben
         echo "Gemessene Temperatur des Sensors ${sensor}: ${temp} °C"
 
+        local request="${SERVER}/api/v1/log/temp/${sensor}/${temp}&apikey=${API_KEY}"
+        local status='error'
         if [ "$#" == '0' ]; then
             # API abfragen
-            request="${SERVER}/api/v1/log/temp/${sensor}/${temp}&apikey=${API_KEY}"
             status=$(curl -X GET "${request}" 2> /dev/null | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["status"]' 2> /dev/null)
         elif [ "$1" == 'show' ]; then
             status='ok'
         fi
 
         if [ "${status}" != 'ok' ]; then
-            echo "Err: ${file} | ${request}" >&2
-            exit 1
+            echo "Error  ${file} (${request})" >&2
+            success=false
         fi
-
     done
+
+    if [ "${success}" != true ]; then
+        exit 1
+    else
+        exit 0
+    fi
 }
 
 if [ "$#" == '0' ]; then
